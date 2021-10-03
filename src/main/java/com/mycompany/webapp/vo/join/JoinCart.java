c6f3d6c7f825d6868d2d01902c8f13818cb6fb43
chaeyeon4u <cy990426@naver.com>
2021-10-03 12:17:26 +0900

    Update: joinCart member join 추가

--- src/main/java/com/mycompany/webapp/vo/join/JoinCart.java
+++ src/main/java/com/mycompany/webapp/vo/join/JoinCart.java
@@ -2,12 +2,14 @@ package com.mycompany.webapp.vo.join;
 
 import com.mycompany.webapp.vo.Brand;
 import com.mycompany.webapp.vo.Cart;
+import com.mycompany.webapp.vo.Member;
 import com.mycompany.webapp.vo.ProductColor;
 import com.mycompany.webapp.vo.ProductCommon;
 import com.mycompany.webapp.vo.ProductStock;
 
 public class JoinCart {
 	private Cart cart;
+	private Member member;
 	private ProductCommon productCommon;
 	private ProductColor productColor;
 	private ProductStock productStock;
