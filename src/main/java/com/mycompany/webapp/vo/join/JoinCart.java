7bda160f600d6f07600a077e788f18c03768ed06
chaeyeon4u <cy990426@naver.com>
2021-10-03 12:02:14 +0900

    Update : cartDAO Create : JoinCart VO

---
+++ src/main/java/com/mycompany/webapp/vo/join/JoinCart.java
@@ -0,0 +1,15 @@
+package com.mycompany.webapp.vo.join;
+
+import com.mycompany.webapp.vo.Brand;
+import com.mycompany.webapp.vo.Cart;
+import com.mycompany.webapp.vo.ProductColor;
+import com.mycompany.webapp.vo.ProductCommon;
+import com.mycompany.webapp.vo.ProductStock;
+
+public class JoinCart {
+	private Cart cart;
+	private ProductCommon productCommon;
+	private ProductColor productColor;
+	private ProductStock productStock;
+	private Brand brand;
+}
