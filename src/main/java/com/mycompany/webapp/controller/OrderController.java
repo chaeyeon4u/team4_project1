014cc8be7e8eaf43e7bc8894ebf4f17e8937cef4
Pyunji <vusguswl@gmail.com>
2021-10-06 03:57:37 +0900

    Update: 장바구니에서 선택된 상품 데이터 바인딩, 파싱, 뷰에 데이터 뿌리기

--- src/main/java/com/mycompany/webapp/controller/OrderController.java
+++ src/main/java/com/mycompany/webapp/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.mycompany.webapp.controller;
 
 import java.security.Principal;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -9,8 +10,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.mycompany.webapp.dto.CartInputs;
+import com.mycompany.webapp.dto.CartProduct;
 import com.mycompany.webapp.service.MemberService;
 import com.mycompany.webapp.service.OrderCompleteService;
 import com.mycompany.webapp.vo.Member;
@@ -21,22 +26,38 @@ import com.mycompany.webapp.vo.join.OrderComplete;
 @RequestMapping("/order")
 public class OrderController {
 	private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
-	
+
 	@Resource MemberService memberService;
-	
-	@RequestMapping("/orderform")
-	public String orderForm(Model model,Principal principal) {
-		//아이디가 들어가는 변수
+
+	@PostMapping("/orderform")
+	public String orderForm(Model model, Principal principal, @ModelAttribute CartInputs cartInputs,
+			Integer totalPrice) {
+		// 아이디가 들어가는 변수
 		String userId = principal.getName();
-		//Mileage 합계를 넣기 위한 변수
+		// Mileage 합계를 넣기 위한 변수
 		int mileageSum = 0;
 		Member member = memberService.memberInfoById(userId);
 		List<Mileage> mileages = memberService.memberMileageById(principal.getName());
-		for(Mileage mileage : mileages) {
+		for (Mileage mileage : mileages) {
 			mileageSum += mileage.getAmount();
 		}
-		model.addAttribute("member",member);
-		model.addAttribute("mileageSum",mileageSum);
+		List<CartProduct> cartProducts = new ArrayList<CartProduct>();
+		for (int i = 0; i < cartInputs.getPcolorId().size(); i++) {
+			CartProduct cartProduct = new CartProduct();
+			cartProduct.setPcolorId(cartInputs.getPcolorId().get(i));
+			cartProduct.setBrandName(cartInputs.getBrandName().get(i));
+			cartProduct.setProductName(cartInputs.getProductName().get(i));
+			cartProduct.setColorCode(cartInputs.getColorCode().get(i));
+			cartProduct.setSizeCode(cartInputs.getSizeCode().get(i));
+			cartProduct.setImg(cartInputs.getImg().get(i));
+			cartProduct.setQuantity(cartInputs.getQuantity().get(i));
+			cartProduct.setAppliedPrice(cartInputs.getAppliedPrice().get(i));
+			cartProducts.add(cartProduct);
+		}
+		model.addAttribute("member", member);
+		model.addAttribute("mileageSum", mileageSum);
+		model.addAttribute("cartProducts", cartProducts);
+		model.addAttribute("totalPrice", totalPrice);
 		/*orderService.orderDeliveryPaymentInfo(null);*/
 		return "order/orderForm";
 	}
@@ -44,38 +65,27 @@ public class OrderController {
 	@RequestMapping("/addressPopup")
 	public String addressPopup() {
 		return "popup/jusoPopup";
-	}	
-	
-	@Resource
-	private OrderCompleteService orderCompleteService;
-	
+	}
+
+	@Resource private OrderCompleteService orderCompleteService;
+
 	@RequestMapping("/ordercomplete")
 	public String orderComplete(Model model, Principal principal, String orderid) {
 		String ordersId = "20211003P1234";
 		String mid = principal.getName();
-		List<OrderComplete> orderProduct= orderCompleteService.selectProductByorderId(mid ,ordersId);
-		List<OrderComplete> orderpayment= orderCompleteService.selectpaymentByorderId(mid,ordersId);
-		List<OrderComplete> orderaddress= orderCompleteService.selectaddressByorderId(mid,ordersId);
-		
+		List<OrderComplete> orderProduct = orderCompleteService.selectProductByorderId(mid, ordersId);
+		List<OrderComplete> orderpayment = orderCompleteService.selectpaymentByorderId(mid, ordersId);
+		List<OrderComplete> orderaddress = orderCompleteService.selectaddressByorderId(mid, ordersId);
+
 		String userId = principal.getName();
 		Member member = memberService.memberInfoById(userId);
-		
-		model.addAttribute("member",member);
-		model.addAttribute("orderProduct",orderProduct);
-		model.addAttribute("orderpayment",orderpayment);
-		model.addAttribute("orderaddress",orderaddress);
+
+		model.addAttribute("member", member);
+		model.addAttribute("orderProduct", orderProduct);
+		model.addAttribute("orderpayment", orderpayment);
+		model.addAttribute("orderaddress", orderaddress);
 		logger.info("orderAddress:" + orderaddress);
 		logger.info("실행");
 		return "order/orderComplete";
 	}
 }
-
-
-
-
-
-
-
-
-
-
